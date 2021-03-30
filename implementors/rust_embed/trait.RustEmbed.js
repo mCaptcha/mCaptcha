b1161c26c99ae7a1a809a12796b819e162e130ba
realaravinth <realaravinth@batsense.net>
2021-03-30 15:35:13 +0000

    Deploying to gh-pages from  @ a3a118927e3024bf9597890fb1c7b1e187e7c8cd 🚀

---
+++ implementors/rust_embed/trait.RustEmbed.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["guard"] = [{"text":"impl RustEmbed for <a class=\"struct\" href=\"guard/docs/struct.Asset.html\" title=\"struct guard::docs::Asset\">Asset</a>","synthetic":false,"types":["guard::docs::Asset"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
