d668f1b0e2a3c201b737b0167d062ae3bc47f0b6
realaravinth <realaravinth@batsense.net>
2021-03-31 07:11:50 +0000

    Deploying to gh-pages from  @ e0dab001aa07ed4b731ab5bae1a0711202c747d4 🚀

--- guard/api/v1/meta/sidebar-items.js
+++ guard/api/v1/meta/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Health","Health check return datatype"],["HealthBuilder","Builder for `Health`."],["build_details",""],["health",""]]});
+initSidebarItems({"struct":[["BuildDetails",""],["BuildDetailsBuilder","Builder for `BuildDetails`."],["Health","Health check return datatype"],["HealthBuilder","Builder for `Health`."],["build_details",""],["health",""]]});
