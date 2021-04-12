4d93ca076281753f608ee22e7c552952bf9c68db
realaravinth <realaravinth@batsense.net>
2021-04-12 05:30:02 +0000

    Deploying to gh-pages from  @ 1b0dfd8082986252aba4bb79ec2ac8a6c42d88e6 🚀

--- guard/api/v1/pow/sidebar-items.js
+++ guard/api/v1/pow/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["services",""]],"mod":[["get_config",""],["verify_pow",""],["verify_token",""]]});
+initSidebarItems({"fn":[["intenral_services",""],["services",""]],"mod":[["get_config",""],["verify_pow",""],["verify_token",""]]});
