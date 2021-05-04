3885bb39ca4e6d7a7ffa14bd2c2ed9d72fd95e9a
realaravinth <realaravinth@batsense.net>
2021-05-04 14:11:06 +0000

    Deploying to gh-pages from  @ 98719670dfd1bd29d8751a277b62f77836a0117e 🚀

--- guard/pages/panel/sitekey/list/sidebar-items.js
+++ guard/pages/panel/sitekey/list/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["PAGE",""]],"fn":[["list_sitekeys",""]],"struct":[["IndexPage",""]]});
+initSidebarItems({"constant":[["PAGE",""]],"fn":[["list_sitekeys",""]],"struct":[["IndexPage",""]],"type":[["SiteKeys",""]]});
