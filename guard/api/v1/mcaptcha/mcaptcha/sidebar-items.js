55bf2fef9c063c7e2add5a87f021b41a62a2673b
realaravinth <realaravinth@batsense.net>
2021-05-06 08:43:25 +0000

    Deploying to gh-pages from  @ 30f457ca431967fd89c0d6004be24053017e3a3d 🚀

--- guard/api/v1/mcaptcha/mcaptcha/sidebar-items.js
+++ guard/api/v1/mcaptcha/mcaptcha/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["add_mcaptcha",""],["add_mcaptcha_util",""],["delete_mcaptcha",""],["get_token",""],["services",""],["update_token",""],["update_token_helper",""]],"mod":[["routes",""]],"struct":[["MCaptchaDetails",""],["MCaptchaID",""]]});
+initSidebarItems({"fn":[["add_mcaptcha_util",""],["delete_mcaptcha",""],["get_token",""],["services",""],["update_token",""],["update_token_helper",""]],"mod":[["routes",""]],"struct":[["MCaptchaDetails",""],["MCaptchaID",""]]});
