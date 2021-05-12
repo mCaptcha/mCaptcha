408cb86a68ea1499536379d993183bbb92aa52ae
realaravinth <realaravinth@batsense.net>
2021-05-12 12:14:50 +0000

    Deploying to gh-pages from  @ a4b409e91441cd4c7624969bf5042cbb91f85d0a 🚀

--- guard/api/v1/pow/get_config/sidebar-items.js
+++ guard/api/v1/pow/get_config/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["get_config","get PoW configuration for an mcaptcha key"],["init_mcaptcha","Call this when [MCaptcha][m_captcha::MCaptcha] is not in master."]],"struct":[["GetConfigPayload",""],["PoWConfig",""]]});
+initSidebarItems({"fn":[["get_config","get PoW configuration for an mcaptcha key"],["init_mcaptcha","Call this when [MCaptcha][m_captcha::MCaptcha] is not in master."]],"struct":[["GetConfigPayload",""]]});
