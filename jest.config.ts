6eb75d7a66177b80e448629f1dab62c1ade8c903
realaravinth <realaravinth@batsense.net>
2021-10-13 10:37:51 +0530

    upgrade dev dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -133,7 +133,7 @@ export default {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-jsdom",
+  testEnvironment: "jest-environment-jsdom",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
