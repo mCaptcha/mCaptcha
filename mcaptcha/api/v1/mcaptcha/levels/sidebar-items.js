485ff66259a9bf1b37cb223f6a6e5bbcf8bc7abc
realaravinth <realaravinth@batsense.net>
2021-12-03 09:15:08 +0000

    Deploying to gh-pages from  @ 54b14291ec140ea4cbbf73462d3d6fc2d39f2d2c 🚀

--- mcaptcha/api/v1/mcaptcha/levels/sidebar-items.js
+++ mcaptcha/api/v1/mcaptcha/levels/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["get_levels_util",""],["services",""]],"mod":[["routes",""]],"struct":[["AddLevels",""],["I32Levels",""],["Levels",""],["UpdateLevels",""],["add_levels",""],["get_levels",""],["update_levels",""]]});
+initSidebarItems({"fn":[["add_captcha_runner",""],["get_levels_util",""],["services",""]],"mod":[["routes",""]],"struct":[["AddLevels",""],["I32Levels",""],["Levels",""],["UpdateLevels",""],["add_levels",""],["get_levels",""],["update_levels",""]]});
--- tests_migrate/settings/sidebar-items.js
+++ tests_migrate/settings/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["check_url",""],["set_database_url",""],["set_from_database_url",""]],"struct":[["Captcha",""],["Database",""],["DatabaseBuilder",""],["Redis",""],["Server",""],["Settings",""],["Smtp",""]]});
+initSidebarItems({"fn":[["check_url",""],["set_database_url",""],["set_from_database_url",""]],"struct":[["Captcha",""],["Database",""],["DatabaseBuilder",""],["DefaultDifficultyStrategy",""],["Redis",""],["Server",""],["Settings",""],["Smtp",""]]});
