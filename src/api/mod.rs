fc8a1670d230fe005f67675d656fbadd8afbba33
realaravinth <realaravinth@batsense.net>
2022-01-08 22:16:05 +0530

    update copyright notice

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2021  Aravinth Manivannan <realaravinth@batsense.net>
+* Copyright (C) 2022  Aravinth Manivannan <realaravinth@batsense.net>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU Affero General Public License as
