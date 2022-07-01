d9976043e8fd7b429cf15d76696b0bc0d1a7a21b
realaravinth <realaravinth@users.noreply.github.com>
2022-07-01 09:29:52 +0000

    Deploying to gh-pages from  @ 5daeffd6fbd421b0bffa4111f62ae3ac8f663bff 🚀

--- implementors/db_core/trait.CloneSPDatabase.js
+++ implementors/db_core/trait.CloneSPDatabase.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
 implementors["db_core"] = [];
+implementors["db_sqlx_postgres"] = [];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
