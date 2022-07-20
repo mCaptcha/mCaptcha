b8d3b1449acb1ad9375c41f89f7147405ed4a316
realaravinth <realaravinth@batsense.net>
2022-07-20 17:41:39 +0530

    feat: adapt db/db-migrations to run mariadb migrations

--- db/db-migrations/src/main.rs
+++ db/db-migrations/src/main.rs
@@ -17,12 +17,14 @@
 use std::env;
 
 use sqlx::postgres::PgPoolOptions;
+use sqlx::mysql::MySqlPoolOptions;
 
 #[cfg(not(tarpaulin_include))]
 #[actix_rt::main]
 async fn main() {
     //TODO featuregate sqlite and postgres
     postgres_migrate().await;
+    maria_migrate().await;
 }
 
 async fn postgres_migrate() {
@@ -34,6 +36,20 @@ async fn postgres_migrate() {
         .expect("Unable to form database pool");
 
     sqlx::migrate!("../db-sqlx-postgres/migrations/")
+        .run(&db)
+        .await
+        .unwrap();
+}
+
+async fn maria_migrate() {
+    let db_url = env::var("MARIA_DATABASE_URL").expect("set POSTGRES_DATABASE_URL env var");
+    let db = MySqlPoolOptions::new()
+        .max_connections(2)
+        .connect(&db_url)
+        .await
+        .expect("Unable to form database pool");
+
+    sqlx::migrate!("../db-sqlx-maria/migrations/")
         .run(&db)
         .await
         .unwrap();
