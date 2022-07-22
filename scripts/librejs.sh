c5dc72a83c65de370a0a1a23a9c963d92560a89a
realaravinth <realaravinth@batsense.net>
2022-07-23 00:47:44 +0530

    fix: don't escape license prefixes and script content

--- scripts/librejs.sh
+++ scripts/librejs.sh
@@ -16,19 +16,19 @@ print_license_msg() {
 
 apply_agpl() {
 	print_license_msg "AGPL" $1
-	local AGPL="// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0"
+	local AGPL='// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0'
 	echo $AGPL >> $1
 }
 
 apply_x11() {
 	print_license_msg "X11" $1
-	local MIT="// @license magnet:?xt=urn:btih:5305d91886084f776adcf57509a648432709a7c7&dn=x11.txt X11"
+	local MIT='// @license magnet:?xt=urn:btih:5305d91886084f776adcf57509a648432709a7c7&dn=x11.txt X11'
 	echo $MIT >> $1
 }
 
 apply_apache() {
 	print_license_msg "APACHE" $1
-	local APACHE="// @license magnet:?xt=urn:btih:8e4f440f4c65981c5bf93c76d35135ba5064d8b7&dn=apache-2.0.txt Apache-2.0"
+	local APACHE='// @license magnet:?xt=urn:btih:8e4f440f4c65981c5bf93c76d35135ba5064d8b7&dn=apache-2.0.txt Apache-2.0'
 	echo $APACHE >> $1
 }
 
@@ -38,7 +38,8 @@ msg "${BLUE}[*] LibreJS processor running"
 
 for file in $(find $DIST  -type f -a -name "*.js")
 do
-	contents=$(cat $file)
+	contents=""
+	cp $file $file.librejs
 	: > $file
 
 	name=$(get_file_name $file)
@@ -57,6 +58,7 @@ do
 	esac
 
 	echo $SOURCE >> $file
-	echo $contents >> $file
+	cat $file.librejs >> $file
+	rm $file.librejs
 	echo $LICENSE_END >> $file
 done
