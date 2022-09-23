af485c750de5d3d4bf4755b8761b2ebd697d2b4f
realaravinth <realaravinth@users.noreply.github.com>
2022-09-23 09:55:34 +0000

    Deploying to gh-pages from  @ d506d291c375785257bc8046bc980313427392d9 🚀

--- source-files.js
+++ source-files.js
@@ -1,6 +1,7 @@
-var sourcesIndex = {};
-sourcesIndex["db_core"] = {"name":"","files":["errors.rs","lib.rs","ops.rs","tests.rs"]};
-sourcesIndex["db_sqlx_maria"] = {"name":"","files":["errors.rs","lib.rs"]};
-sourcesIndex["db_sqlx_postgres"] = {"name":"","files":["errors.rs","lib.rs"]};
-sourcesIndex["mcaptcha"] = {"name":"","dirs":[{"name":"api","dirs":[{"name":"v1","dirs":[{"name":"account","files":["delete.rs","email.rs","mod.rs","password.rs","secret.rs","username.rs"]},{"name":"mcaptcha","files":["create.rs","delete.rs","easy.rs","get.rs","mod.rs","stats.rs","update.rs"]},{"name":"notifications","files":["add.rs","get.rs","mark_read.rs","mod.rs"]},{"name":"pow","files":["get_config.rs","mod.rs","verify_pow.rs","verify_token.rs"]}],"files":["auth.rs","meta.rs","mod.rs","routes.rs"]}],"files":["mod.rs"]},{"name":"email","files":["mod.rs","verification.rs"]},{"name":"pages","dirs":[{"name":"auth","files":["login.rs","mod.rs","register.rs","sudo.rs"]},{"name":"panel","dirs":[{"name":"sitekey","files":["add.rs","delete.rs","edit.rs","list.rs","mod.rs","view.rs"]}],"files":["mod.rs","notifications.rs","settings.rs"]}],"files":["errors.rs","mod.rs","routes.rs","sitemap.rs"]},{"name":"static_assets","files":["filemap.rs","mod.rs","static_files.rs"]},{"name":"widget","files":["mod.rs"]}],"files":["data.rs","date.rs","db.rs","demo.rs","docs.rs","errors.rs","main.rs","routes.rs","settings.rs","stats.rs"]};
+var sourcesIndex = JSON.parse('{\
+"db_core":["",[],["errors.rs","lib.rs","ops.rs","tests.rs"]],\
+"db_sqlx_maria":["",[],["errors.rs","lib.rs"]],\
+"db_sqlx_postgres":["",[],["errors.rs","lib.rs"]],\
+"mcaptcha":["",[["api",[["v1",[["account",[],["delete.rs","email.rs","mod.rs","password.rs","secret.rs","username.rs"]],["mcaptcha",[],["create.rs","delete.rs","easy.rs","get.rs","mod.rs","stats.rs","update.rs"]],["notifications",[],["add.rs","get.rs","mark_read.rs","mod.rs"]],["pow",[],["get_config.rs","mod.rs","verify_pow.rs","verify_token.rs"]]],["auth.rs","meta.rs","mod.rs","routes.rs"]]],["mod.rs"]],["email",[],["mod.rs","verification.rs"]],["pages",[["auth",[],["login.rs","mod.rs","register.rs","sudo.rs"]],["panel",[["sitekey",[],["add.rs","delete.rs","edit.rs","list.rs","mod.rs","view.rs"]]],["mod.rs","notifications.rs","settings.rs"]]],["errors.rs","mod.rs","routes.rs","sitemap.rs"]],["static_assets",[],["filemap.rs","mod.rs","static_files.rs"]],["widget",[],["mod.rs"]]],["data.rs","date.rs","db.rs","demo.rs","docs.rs","errors.rs","main.rs","routes.rs","settings.rs","stats.rs"]]\
+}');
 createSourceSidebar();
