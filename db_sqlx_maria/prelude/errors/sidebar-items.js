2b324f833427f9ef52e2793057c51823c6f49b28
realaravinth <realaravinth@users.noreply.github.com>
2023-03-10 09:49:29 +0000

    Deploying to gh-pages from  @ 31d12206aa90bfe187e2130b7338d38761a1bfeb 🚀

--- db_sqlx_maria/prelude/errors/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"enum":[["DBError","Error data structure grouping various error subtypes"]],"type":[["BoxDynError","Convenience type alias for grouping driver-specific errors"],["DBResult","Generic result data structure"]]};
