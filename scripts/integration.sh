5324969bd29d25e6d1186bc825f9d9eadfcb0a76
Aravinth Manivannan <realaravinth@batsense.net>
2023-04-30 21:10:59 +0530

    feat: install selenium drivers

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -7,13 +7,33 @@ readonly PROJECT_ROOT=$(realpath $(dirname $(dirname "${BASH_SOURCE[0]}")))
 
 source $PROJECT_ROOT/scripts/lib.sh
 
+is_ci(){
+    if [ -z ${CI+x} ];
+    then
+        return 1
+    else
+        return 0
+    fi
+}
+
 
 
 docker-compose down -v --remove-orphans || true
 docker-compose up -d
 cd $(mktemp -d)
+pwd
+find 
 git clone https://github.com/mCaptcha/integration . 
-yarn install
-npx nightwatch ./test/mCaptcha.ts
+
+if is_ci
+then
+	yarn install
+	xvfb-run --auto-servernum npm run test.chrome
+	xvfb-run --auto-servernum npm run test.firefox
+else
+	yarn install
+	npx nightwatch ./test/mCaptcha.ts
+fi
+
 cd $PROJECT_ROOT
 docker-compose down -v --remove-orphans || true
